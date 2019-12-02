d070cab8d5dec825c7f135387536a68834682742
kiranbalampaki <kiranmgr1996@gmail.com>
2019-12-02 11:25:13 +0545

    Login and dashboard

--- app/src/main/java/com/example/patientcaresystem/MainActivity.java
+++ app/src/main/java/com/example/patientcaresystem/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.patientcaresystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etUsername, etPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = String.valueOf(etUsername.getText());
+                String password = String.valueOf(etPassword.getText());
+
+                //Toast.makeText(getApplicationContext(),password,Toast.LENGTH_SHORT).show();
+                if (String.valueOf(etUsername.getText()).equals("admin") && String.valueOf(etPassword.getText()).equals("admin")){
+                    Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(),"Fail",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
